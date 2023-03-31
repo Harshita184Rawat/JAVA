acf35dbd39148219a493fb78083ac1148244cfe5
Harshita Rawat <harshita.reenu184@gmail.com>
2023-03-31 23:35:47 +0530

    Add files via upload

---
+++ StringBuilder.java
@@ -0,0 +1,28 @@
+
+public class StringBuilder {
+    public static void main(String args[]) {
+        String str = "Keep coding";
+        // declaring a stringBuilder
+        StringBuilder sb = new StringBuilder(str);
+
+        // to get character from string
+        System.out.print(sb.charAt(0));
+
+        // to set character at specific index
+        System.out.print(sb.SetCharAt(0, 'o');
+
+        // to insert char at specific index
+
+    
+       // to delete a char/substring
+       sb.delete(0,5);
+       System.out.print(sb);
+
+       sb.append(" Re");
+       System.out.print(sb);
+       
+
+
+
+    }
+}
