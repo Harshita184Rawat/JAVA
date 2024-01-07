09343fd497eb8a662700f72e2c40d13a87824904
Harshita Rawat <harshita.reenu184@gmail.com>
2024-01-07 20:54:14 +0530

    Update FactorialUsingRecursion.java

--- FactorialUsingRecursion.java
+++ FactorialUsingRecursion.java
@@ -1,24 +1,19 @@
 import java.util.*;
 
 public class FactorialUsingRecursion {
-    public static void fact(int number, int factorial, int n) {
-        factorial = factorial * number;
-        if (number == 0 || number == 1) {
-            System.out.println("Factorial is : " + factorial);
-        } else if (number <= n) {
-            fact(number - 1, factorial, n);
-
-        }
-        System.out.println("Factorial is : " + factorial);
+    public static int fact(int number) {
+        if (number == 0 || number == 1)
+            return 1;
+        int factorialpre = fact(number - 1);
+        int factorial_n = number * factorialpre;
+        return factorial_n;
     }
 
     public static void main() {
         int number;
         Scanner sc = new Scanner(System.in);
         number = sc.nextInt();
-        int fatorial = 1;
         int n = number;
-        fact(number, fatorial, n);
-
+        fact(number);
     }
 }
