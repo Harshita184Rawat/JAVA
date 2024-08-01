b1ed15ed74b959820816fa7e61d13c0fa79dd463
Harshita Rawat <harshita.reenu184@gmail.com>
2024-08-01 16:39:28 +0530

    Add files via upload

---
+++ trees/HeightOfTree.java
@@ -0,0 +1,64 @@
+public class HeightOfTree {
+    // To find height  tree
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        // constructor of node
+        Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node builTree(int nodes[]){
+            idx++;
+            // return root node
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = builTree(nodes);
+            newNode.right = builTree(nodes);
+            
+            return newNode;
+            
+        }
+     } 
+
+     public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int myHeight = Math.max(leftHeight,rightHeight) + 1;
+
+        return myHeight;
+     }
+
+    public static void main(String[] args) {
+
+        
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1,};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.builTree(nodes);
+        // if tree is build correctly the value of the root will be node[0] i.e 1
+        // System.out.println(root.data);       
+        System.out.println();
+       System.out.println("Height of tree : "+height(root));
+        
+    }
+
+    
+    
+}
