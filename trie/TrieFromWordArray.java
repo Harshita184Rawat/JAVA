ddb67d95d806c10dac99ec241d6e78769c96147b
Harshita Rawat <harshita.reenu184@gmail.com>
2024-08-15 08:10:05 +0530

    Update TrieFromWordArray.java

--- trie/TrieFromWordArray.java
+++ trie/TrieFromWordArray.java
@@ -7,6 +7,10 @@ public class TrieFromWordArray {
         // constructor
         public Node() {
             children = new Node[26]; // array size [a-z]
+            // initilaise each ith node in children array to null: if doesn't work use
+            // for(int i=0;i<26;i++){
+            //     children[i] =null;
+            // }
             endOfWord = false;
         }
     }
